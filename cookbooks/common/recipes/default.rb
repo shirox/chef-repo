81202ea0a24822d81ca544fbf741376430390e6c
shirox <shirota@shirox.com>
2017-02-09 23:10:48 +0900

    first commit

---
+++ cookbooks/common/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: common
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "net-tools" do
+    action :install
+end
+
---
+++ cookbooks/httpd/recipes/default.rb
@@ -0,0 +1,24 @@
+#
+# Cookbook Name:: httpd
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "httpd" do
+  action :install
+end
+
+template "/etc/httpd/conf/httpd.conf" do
+  source "httpd.conf.erb"
+  group "root"
+  owner "root"
+  mode "0644"
+end  
+
+service "httpd" do
+  supports :status => true, :restart => true
+  action [:enable, :start]
+end
