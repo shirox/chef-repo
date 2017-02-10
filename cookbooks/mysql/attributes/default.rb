54e7dad31f1d7e0c20f70ab344afc5c2ea92e656
shirox <shirota@shirox.com>
2017-02-10 21:49:46 +0900

    MySQL basic setting

---
+++ cookbooks/mysql/attributes/default.rb
@@ -0,0 +1,1 @@
+default['mysql']['root_password'] = '{MYSQL_ROOT_PASSWORD}'
--- cookbooks/mysql/recipes/default.rb
+++ cookbooks/mysql/recipes/default.rb
@@ -6,3 +6,16 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+package "mysql mysql-server" do
+  action :install
+end
+
+service "mysqld" do
+  action [:enable, :start]
+end
+
+execute "set mysql root password" do
+  command "mysqladmin -uroot password #{node['mysql']['root_password']}"
+  only_if "mysql -uroot -e 'show databases;'"
+end
